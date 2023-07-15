a7d8773f5885b9e4939d7736e147da6d4071049b
themikail <hello@mikailgokce.com>
2023-07-15 20:26:54 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KanbanBoard from "./kanbanboard";
+import Login from "./log";
+import Signup from "./signup";
 
 function App() {
   return (
-    <div className="App">
-      <KanbanBoard />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<KanbanBoard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
