b06573a161eb5a9348e312b5e9a635b794c9e7e1
themikail <hello@mikailgokce.com>
2023-07-15 20:33:15 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import KanbanBoard from "./kanbanboard";
 import Login from "./log";
 import Signup from "./signup";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Hier können Sie Ihre Logik zur Benutzeranmeldung implementieren
+    // Setzen Sie isLoggedIn auf true, wenn die Anmeldung erfolgreich ist
+    setIsLoggedIn(true);
+  };
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<KanbanBoard />} />
+          <Route
+            path="/login"
+            element={
+              isLoggedIn ? (
+                <Navigate to="/" />
+              ) : (
+                <Login handleLogin={handleLogin} />
+              )
+            }
+          />
+          <Route
+            path="/signup"
+            element={isLoggedIn ? <Navigate to="/" /> : <Signup />}
+          />
+          <Route
+            path="/"
+            element={isLoggedIn ? <KanbanBoard /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
     </Router>
